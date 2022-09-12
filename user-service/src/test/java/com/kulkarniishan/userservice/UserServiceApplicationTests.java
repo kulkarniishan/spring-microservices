b3bb41f0f22c1ca1672aae61dd01e8845c1ad5d9
Ishan <ishan.kulkarni@somaiya.edu>
2022-09-13 03:59:07 +0530

    FEATURE: User service initialized :tada:

---
+++ user-service/src/test/java/com/kulkarniishan/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kulkarniishan.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
